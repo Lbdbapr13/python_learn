c4c64bc580f4b80eb6c6768694c9fcf250099ece
Leonardo Prakoso <111039648+Lbdbapr13@users.noreply.github.com>
2022-09-21 12:46:54 +0700

    Update bool.py

--- bool.py
+++ bool.py
@@ -20,3 +20,21 @@ def can_vote_in_elections(age):
     return age>= 17
 print("can a 16 years old vote in election?",can_vote_in_elections(16))
 print("can a 18 years old vote in election?",can_vote_in_elections(18))
+
+# n % 2 == 1, it's one syntax means "True"
+def is_odd(n):
+    return (n % 2)== 1 
+print("is 100 odd?", is_odd(100))
+print("is -1 odd?", is_odd(-1))
+
+print (3.0 == 3)
+
+#contoh kasus
+def can_run_for_president(age, is_natural_born_citizen):
+    """Can someone of the given age and citizenship status run for president in the Indonesia?"""
+    #The Indonesia Constitution says you must be a natual born citizen *and* at least 40 years old
+    return is_natural_born_citizen and (age>=40)
+print(can_run_for_president(20, True))
+print(can_run_for_president(40, False))
+print(can_run_for_president(35, True))
+print(can_run_for_president(40, True))
