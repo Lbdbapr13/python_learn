07f375b5993602f0843b2c2c02185085523f00b1
Leonardo Prakoso <111039648+Lbdbapr13@users.noreply.github.com>
2022-09-21 12:36:57 +0700

    Update angka.py

--- angka.py
+++ angka.py
@@ -2,3 +2,23 @@ print (3*5)
 print(81/9)
 print(2+3)
 print(11-11)
+#contoh kasus
+# n % 2==1 & n//=2
+#floor division descards the fractional part
+print (9//2)
+# in python the % operator returns the remainder of the division
+print (17 % 3)
+# mencoba menghitung ruas suatu bidang
+width= 30
+height = 2*3
+print(width*height)
+# mencoba menghitung ruas suatu bidang
+width= 30
+height = 2*3
+print(width * height)
+# mencoba harga yang terkena pajak
+total_pajak = 12/100
+harga_barang = 1500
+harga_pajak = harga_barang * total_pajak
+print("Harga pajak=",harga_pajak)
+print(harga_barang+harga_pajak)
