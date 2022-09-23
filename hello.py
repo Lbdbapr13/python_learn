ba369412a92a20e97548c21dbc54f6a31f4d726c
Leonardo Prakoso <111039648+Lbdbapr13@users.noreply.github.com>
2022-09-23 12:13:02 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,18 @@ print("Hello, World!")
 s = "Leonardo Budi Bagas Prakoso"
 print(s)
 print(type(s))
+
+#Study case 1
+print("there once was a man named Jake")
+print("he was 45 years old")
+print("he really liked the name Jake")
+print("but didn' like being 45")
+
+#study case 1 + variable
+#study case 1 + variable
+character_name = "Bara"
+character_age = "13"
+print("there once was a man named " + character_name +",")
+print("he was "+ character_age + "years old")
+print("he really liked the name "+character_name+",")
+print("but didn' like being "+ character_age)
