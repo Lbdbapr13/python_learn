6c1e6e8adc6b2e539834370a0568b7994ec62e33
Leonardo Prakoso <111039648+Lbdbapr13@users.noreply.github.com>
2022-09-23 14:48:48 +0700

    Add files via upload

---
+++ basic_cal.py
@@ -0,0 +1,7 @@
+#first app to do basic calculator
+num1 = input ("enter a number:")
+num2 = input ("enter another number:")
+result= float (num1) + float(num2) 
+#it's best to use float becuase it can contain full and decimal number, not like int 
+print(result)
+
