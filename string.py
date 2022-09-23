eda335974f7e578929ed088ff6c67cc0b2dc2935
Leonardo Prakoso <111039648+Lbdbapr13@users.noreply.github.com>
2022-09-23 12:30:41 +0700

    Create string.py

---
+++ string.py
@@ -0,0 +1,14 @@
+#is one type data in python
+print("Baraebi wordpresss")
+print("baraebi\nwordpress") #"\n" it's one fuction to make string in a new line
+print("Baraebi\"Wordpress") #\" it's use to pronouce "
+
+#string use variable
+kalimat = "Baraebi Wordpress"
+print (kalimat +" is new")
+print (kalimat.upper().islower())
+
+#len - to know how many characters are inside the string
+kalimat = "Baraebi Wordpress"
+print(len(kalimat))
+print(kalimat[0])
