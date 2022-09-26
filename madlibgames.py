a6dc7c7aa07da54b402189e09e83c7de6a2424ae
Leonardo Prakoso <111039648+Lbdbapr13@users.noreply.github.com>
2022-09-26 23:55:46 +0700

    Create madlibgames.py

---
+++ madlibgames.py
@@ -0,0 +1,17 @@
+# second app about mad lib game from freecodecamp.org at youtube
+color = input("Enter a Colour: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+print ("Rose are "+color)
+print(plural_noun +" are blue")
+print("I love " + celebrity)
+
+# it's my own version of madlibs games
+place = input ("Enter a Place: ")
+colour = input ("Enter your Colour: ")
+nearest_thing = input ("Enter a name of thing that nearest from you: ")
+flower = input ("Enter a Flower Name: ")
+beloved_name = input ("Enter a name of your beloved: ")
+print (place+", the place we meet. You wearing a one piece dress with " + colour+ "colour")
+print ("I am like a " + nearest_thing +"or just like a beast and you are a" + flower)
+print("from there "+ beloved_name+" become my priority.")
